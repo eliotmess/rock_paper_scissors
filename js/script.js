570ddaa540fb68dea02b4e4a6b5bcaddf00bc62b
Grzes Olejniczak <olejniczak.grzes@gmail.com>
2018-10-06 00:29:23 +0200

    Add playerMove loop

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 'use strict';
 
-var output = document.getElementById('output').insertAdjacentHTML("afterbegin", "Try yourself and play against real computer - PUSH START!" + '<br>');
+var output = document.getElementById('output');
+output.insertAdjacentHTML("afterbegin", "Try yourself and play against real computer - PUSH START!" + '<br>');
 
 //zmienne
 
-var rockBtn = document.getElementById('rock');
-var paperBtn = document.getElementById('paper');
-var scissorsBtn = document.getElementById('scissors');
+var params = {};
+
 var playerMove;
 var compMove;
 var compValue;
 var duelScore;
-var finalScore;
 var targetScore = 3;
 
 var playerWin = 0;
 var compWin = 0;
 var tableResult = document.getElementById('result').innerHTML= playerWin + ' : ' + compWin;
 
+
+var compAnswer = document.getElementById('comp-answer');
+
 var gameContinue = true;
 
 var newGameBtn = document.getElementById('newgame');
 
 
+
 // funkcja zakończenia gry
 
-finalScore = function() {
+var finalScore = function() {
   if( playerWin == targetScore ) {
-    document.getElementById('output').insertAdjacentHTML("afterbegin", '<br>' + ' YOU WON WITH COMPUTER, FOR NOW ' +  '<br>' + '<br>');
+    output.insertAdjacentHTML("afterbegin", '<br>' + ' YOU WON WITH COMPUTER, FOR NOW ' +  '<br>' + '<br>');
+    compAnswer.innerHTML=' :-/ ';
     gameContinue = false;
     return gameContinue;
-  }
-  else if( compWin == targetScore ) {
-      document.getElementById('output').insertAdjacentHTML("afterbegin", '<br>' + ' YOU LOSE! COMPUTER PLAYED YOU AGAIN ' +  '<br>' + '<br>');
+  } else if( compWin == targetScore ) {
+    output.insertAdjacentHTML("afterbegin", '<br>' + ' YOU LOSE! COMPUTER PLAYED YOU AGAIN ' +  '<br>' + '<br>');
+    compAnswer.innerHTML=' ;-) ';
     gameContinue = false;
     return gameContinue;
   };
@@ -40,83 +44,50 @@ finalScore = function() {
 
 //funkcje przycisków ruchów
 
-rockBtn.addEventListener('click', function(event){
-  
-  if(gameContinue === true){
-    playerMove = ' ROCK ';
-    var compChoice = compMove();
-    var roundScore = duelScore();
-    var countingScore = finalScore();
+var moveBtns = document.querySelectorAll('.player-move');
+
+for (var i = 0; i < moveBtns.length; i++){
+  moveBtns[i].addEventListener('click', function(){
+    if(gameContinue === true){
+      playerMove = this.getAttribute('data-move').toUpperCase();
+      compMove();
+      duelScore();
+      finalScore();
+    } else {
+      output.insertAdjacentHTML("afterbegin", ' Start new game ' +  '<br>');
     }
-  else if(gameContinue === false){
-    document.getElementById('output').insertAdjacentHTML("afterbegin", ' Start new game ' +  '<br>');
-  }
-});
-
-paperBtn.addEventListener('click', function(event){
-  
-  if(gameContinue === true){
-    playerMove = ' PAPER ';
-    var compChoice = compMove();
-    var roundScore = duelScore();
-    var countingScore = finalScore();
-   }
-  else if(gameContinue === false){
-    document.getElementById('output').insertAdjacentHTML("afterbegin", ' Start new game ' +  '<br>');
-  }
-});
-
-scissorsBtn.addEventListener('click', function(event){
-  
-  if(gameContinue === true){
-    playerMove = ' SCISSORS ';
-    var compChoice = compMove();
-    var roundScore = duelScore();
-    var countingScore = finalScore();
-  }
-  else if(gameContinue === false){
-    document.getElementById('output').insertAdjacentHTML("afterbegin", ' Start new game ' +  '<br>');
-  }
-});
-
+  });
+}
 
 // Generator ruchu komputera
 
 compMove = function(event) {
 
-  var compAnswer = document.getElementById('comp-answer');
-  var compMove = Math.random();
-  compMove = Math.floor( compMove * 3 + 1 );
+  var compMove = Math.floor( Math.random() * 3 + 1 );
 
   if (compMove === 1) {
-    compAnswer.innerHTML=' PAPER ';
-    compValue = ' PAPER ';
+    compValue = 'PAPER';
+  } else if (compMove === 2) {
+    compValue = 'ROCK';
+  } else {
+    compValue = 'SCISSORS';
   }
-  else if (compMove === 2) {
-    compAnswer.innerHTML=' ROCK ';
-    compValue = ' ROCK ';
-  }
-  else if (compMove === 3) {
-    compAnswer.innerHTML=' SCISSORS ';
-    compValue = ' SCISSORS ';
-  }
+  compAnswer.innerHTML = compValue;
 };
 
 // PORÓWNANIE WYNIKÓW
 
 duelScore = function() {
-  if ( playerMove === compValue ) {
-     document.getElementById('output').insertAdjacentHTML("afterbegin", ' It\'s a tie! ' + ' You chose ' +  playerMove + ' and computer guess what - ' + compValue +  '<br>');
-  }
-  else if ( (playerMove === ' ROCK ' && compValue === ' PAPER ') || (playerMove === ' PAPER ' && compValue === ' SCISSORS ') || (playerMove === ' SCISSORS ' && compValue === ' ROCK ') ) {
-    document.getElementById('output').insertAdjacentHTML("afterbegin", ' You Lose! ' + ' You chose ' +  playerMove + ' and computer got you good with ' + compValue +  '<br>');
-    compWin += 1;
+  if ( (playerMove === 'PAPER' && compValue === 'ROCK') || (playerMove === 'SCISSORS' && compValue === 'PAPER') || (playerMove === 'ROCK' && compValue === 'SCISSORS') ) {
+    output.insertAdjacentHTML("afterbegin", ' You Win! ' + ' You chose ' +  playerMove + ' and played computer\'s ' + compValue +  '<br>');
+    playerWin ++;
     tableResult = document.getElementById('result').innerHTML= playerWin + ' : ' + compWin;
-  }
-  else if ( (playerMove === ' PAPER ' && compValue === ' ROCK ') || (playerMove === ' SCISSORS ' && compValue === ' PAPER ') || (playerMove === ' ROCK ' && compValue === ' SCISSORS ') ) {
-    document.getElementById('output').insertAdjacentHTML("afterbegin", ' You Win! ' + ' You chose ' +  playerMove + ' and played computer\'s ' + compValue +  '<br>');
-    playerWin += 1;
+  } else if ( (playerMove === 'ROCK' && compValue === 'PAPER') || (playerMove === 'PAPER' && compValue === 'SCISSORS') || (playerMove === 'SCISSORS' && compValue === 'ROCK') ) {
+    output.insertAdjacentHTML("afterbegin", ' You Lose! ' + ' You chose ' +  playerMove + ' and computer got you good with ' + compValue +  '<br>');
+    compWin ++;
     tableResult = document.getElementById('result').innerHTML= playerWin + ' : ' + compWin;
+  } else {
+    output.insertAdjacentHTML("afterbegin", ' It\'s a tie! ' + ' You chose ' +  playerMove + ' and computer guess what - ' + compValue +  '<br>');
   }
 };
 
@@ -127,19 +98,19 @@ newGameBtn.addEventListener('click', function() {
     gameContinue = true;
     playerWin = 0;
     compWin = 0;
+    compAnswer.innerHTML='';
     tableResult = document.getElementById('result').innerHTML= playerWin + ' : ' + compWin;
-    targetScore = window.prompt('Put number of victories to win it all');
+    targetScore = Math.round(window.prompt('Put number of victories to win it all'));
     
     if(isNaN(targetScore) || targetScore === null || targetScore === ''){
       alert('Don\'t make computer wait - put a correct number');
     }
     else if( targetScore <= 0 ){
-        alert('Be serious!');
+      alert('Be serious!');
     }
     else {
-        gameTarget = document.getElementById('gameend').innerHTML= 'YOU PLAY TILL ' + targetScore + ' WINS. MAY THE BEST MAN WIN!';
+      document.getElementById('gameend').innerHTML= 'YOU PLAY TILL ' + targetScore + ' WINS. MAY THE BEST MAN WIN!';
     }
     
     
   });
-
